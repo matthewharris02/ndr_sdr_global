da9fc0090fc9944b85ec537c450e567aa37fc429
Rich Sharp <richpsharp@gmail.com>
2022-09-09 13:59:05 -0700

    addint tracking for config file list

--- run_ndr_sdr_pipeline.py
+++ run_ndr_sdr_pipeline.py
@@ -939,6 +939,13 @@ def _run_ndr(
 
 def main():
     """Entry point."""
+    parser = argparse.ArgumentParser(description='run NDR/SDR pipeline')
+    parser.add_argument('config_file_path_pattern', nargs='+', help='Path to one or more .ini files or matching patterns')
+    args = parser.parse_args()
+    config_file_list = [path for pattern in pattern_list for path in glob.glob(pattern) ]
+
+
+
     task_graph = taskgraph.TaskGraph(
         WORKSPACE_DIR, multiprocessing.cpu_count(), 15.0,
         parallel_mode='process', taskgraph_name='run pipeline main')
